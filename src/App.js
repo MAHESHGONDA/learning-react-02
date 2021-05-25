26ab8ae10aa664c621e2af23ed0c194555e984af
Mahesh Gonda <mahesh.gonda@sap.com>
2021-05-25 19:10:59 +0530

    added use effec fetch example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import UseStateBasic from './components/usestate/UseStateBasic'
 import UseStateArray from './components/usestate/UseStateArray'
 import UseEffectWithDependency from './components/useeffect/UseEffectWithDependency'
+import UseEffectFetch from './components/useeffect/UseEffectFetch'
 
 function App() {
   return (
     <React.Fragment>
       <div className='container'>
-        <UseEffectWithDependency></UseEffectWithDependency>
+        <UseEffectFetch></UseEffectFetch>
       </div>
     </React.Fragment>
   )
---
+++ src/components/useeffect/UseEffectFetch.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+
+const url = 'https://api.github.com/users'
+const UseEffectFetch = () => {
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    console.log(users)
+    setUsers(users)
+  }
+
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    getUsers()
+  }, [])
+  return (
+    <>
+      <h1>Github Users</h1>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default UseEffectFetch
