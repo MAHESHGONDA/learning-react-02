443d58791a9e4a56336f6b2d3a50af9174ee5e44
Mahesh Gonda <mahesh.gonda@sap.com>
2021-06-11 18:55:55 +0530

    added fetch

--- src/components/useeffect/UseEffectFetch.js
+++ src/components/useeffect/UseEffectFetch.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
-const url = 'https://api.github.com/users'
+const url = 'https://api.github.com/users';
 const UseEffectFetch = () => {
   const getUsers = async () => {
-    const response = await fetch(url)
-    const users = await response.json()
-    console.log(users)
-    setUsers(users)
-  }
+    const response = await fetch(url);
+    const users = await response.json();
+    console.log(users);
+    setUsers(users);
+  };
 
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    getUsers()
-  }, [])
+    getUsers();
+  }, []);
   return (
     <>
       <h1>Github Users</h1>
       <ul className='users'>
         {users.map((user) => {
-          const { id, login, avatar_url, html_url } = user
+          const { id, login, avatar_url, html_url } = user;
           return (
             <li key={id}>
               <img src={avatar_url} alt={login} />
@@ -26,11 +26,11 @@ const UseEffectFetch = () => {
                 <h4>{login}</h4>
               </div>
             </li>
-          )
+          );
         })}
       </ul>
     </>
-  )
-}
+  );
+};
 
-export default UseEffectFetch
+export default UseEffectFetch;
